eb8df9b8f9dee99518017ccaa303b655a43dbcc5
Mohamed Taha <mohamed.a.taha55555@gmail.com>
2026-02-06 18:37:05 +0200

    edit deployOnK8s

--- vars/deployOnK8s.groovy
+++ vars/deployOnK8s.groovy
@@ -1,12 +1,12 @@
+// vars/deployOnK8s.groovy
 def call(String deploymentFile) {
-
     withCredentials([
         string(credentialsId: 'API-SERVER', variable: 'API'),
         string(credentialsId: 'Jenkins-Token', variable: 'TOKEN')
     ]) {
-        sh '''
-            kubectl --server="${API}" --token="${TOKEN}" --insecure-skip-tls-verify apply -f "${deploymentFile}" -n ivolve
-            kubectl --server="${API}" --token="${TOKEN}" --insecure-skip-tls-verify get pods -n ivolve
-        '''
+        sh """
+            kubectl --server="\\${API}" --token="\\${TOKEN}" --insecure-skip-tls-verify apply -f "${deploymentFile}" -n ivolve
+            kubectl --server="\\${API}" --token="\\${TOKEN}" --insecure-skip-tls-verify get pods -n ivolve
+        """
     }
 }
