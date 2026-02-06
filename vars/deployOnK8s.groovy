ec2cecfc52d5f73e90893181d61c36ed3cf496be
Mohamed Taha <mohamed.a.taha55555@gmail.com>
2026-02-06 17:10:27 +0200

    add cred.

--- vars/deployOnK8s.groovy
+++ vars/deployOnK8s.groovy
@@ -1,4 +1,8 @@
 def call(String filePath) {
+    withCredentials([
+        string(credentialsId: tokenCredID, variable: 'Jenkins-Token'),
+        string(credentialsId: apiServerCredID, variable: 'API-SERVER')
+    ])
     echo "Deploying to Kubernetes"
     sh "kubectl apply -f ${filePath}"
 }
