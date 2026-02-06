768d1564baa828f7a04dc6179001dac0247fbd72
Mohamed Taha <mohamed.a.taha55555@gmail.com>
2026-02-06 17:45:38 +0200

    edit deployOnK8s

--- vars/deployOnK8s.groovy
+++ vars/deployOnK8s.groovy
@@ -1,11 +1,11 @@
 def deployOnK8s(String deploymentFile) {
-    withCredentials([
-        string(credentialsId: 'API-SERVER', variable: 'API'),
-        string(credentialsId: 'Jenkins-Token', variable: 'TOKEN')
-    ]) {
-        sh """
-        kubectl --server=$API --token=$TOKEN apply -f $deploymentFile
-        kubectl --server=$API --token=$TOKEN get pods
-        """
+    steps {
+        withCredentials([string(credentialsId: 'API-SERVER', variable: 'API'),
+                         string(credentialsId: 'Jenkins-Token', variable: 'TOKEN')]) {
+            sh """
+            kubectl --server=$API --token=$TOKEN apply -f $deploymentFile
+            kubectl --server=$API --token=$TOKEN get pods
+            """
+        }
     }
 }
